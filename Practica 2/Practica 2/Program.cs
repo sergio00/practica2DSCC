3e61ad07a8dfab15ac4d21f4238a4cfe08103c17
unknown <labproto2@CTI_THIN_54_EN.cti.espol.edu.ec>
2015-11-09 16:19:26 -0500

    Adding Streaming action to the camera

--- Practica 2/Practica 2/Program.cs
+++ Practica 2/Practica 2/Program.cs
@@ -11,30 +11,39 @@ using Microsoft.SPOT.Touch;
 using Gadgeteer.Networking;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
 
 namespace Practica_2
 {
     public partial class Program
     {
-        // This method is run when the mainboard is powered up or reset.   
+
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
             
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
+            camera.CameraConnected += camera_CameraConnected;
+            camera.BitmapStreamed += camera_BitmapStreamed;
 
 
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
+            
+        }
+
+        void camera_BitmapStreamed(Camera sender, Bitmap e)
+        {
+            displayT35.SimpleGraphics.DisplayImage(e, 0, 0);
+        }
+
+        void camera_CameraConnected(Camera sender, EventArgs e)
+        {
+            camera.StartStreaming();
+        }
+
+
+        void button_press(Button sender, Button.ButtonState state){
+            camera.TakePicture();
+
         }
     }
 }
--- Practica 2/Practica 2/Program.generated.cs
+++ Practica 2/Practica 2/Program.generated.cs
@@ -15,6 +15,21 @@ namespace Practica_2 {
     
     public partial class Program : Gadgeteer.Program {
         
+        /// <summary>The SD Card module using socket 5 of the mainboard.</summary>
+        private Gadgeteer.Modules.GHIElectronics.SDCard sdCard;
+        
+        /// <summary>The Button module using socket 11 of the mainboard.</summary>
+        private Gadgeteer.Modules.GHIElectronics.Button button;
+        
+        /// <summary>The Camera module using socket 3 of the mainboard.</summary>
+        private Gadgeteer.Modules.GHIElectronics.Camera camera;
+        
+        /// <summary>The Display T35 module using sockets 14, 13, 12 and 10 of the mainboard.</summary>
+        private Gadgeteer.Modules.GHIElectronics.DisplayT35 displayT35;
+        
+        /// <summary>The USB Client DP module using socket 1 of the mainboard.</summary>
+        private Gadgeteer.Modules.GHIElectronics.USBClientDP usbClientDP;
+        
         /// <summary>This property provides access to the Mainboard API. This is normally not necessary for an end user program.</summary>
         protected new static GHIElectronics.Gadgeteer.FEZSpider Mainboard {
             get {
@@ -37,6 +52,11 @@ namespace Practica_2 {
         }
         
         private void InitializeModules() {
+            this.sdCard = new GTM.GHIElectronics.SDCard(5);
+            this.button = new GTM.GHIElectronics.Button(11);
+            this.camera = new GTM.GHIElectronics.Camera(3);
+            this.displayT35 = new GTM.GHIElectronics.DisplayT35(14, 13, 12, 10);
+            this.usbClientDP = new GTM.GHIElectronics.USBClientDP(1);
         }
     }
 }
